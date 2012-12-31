6a1cfd269715fef281860c5f9a15717187280011
johndavid400 <jd@isotope11.com>
2012-12-31 11:44:08 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deltafin::Application.config.secret_token = '0d2fcd99c5cfc009c4fb17d620aa08d2baf8ee6887b7098b96670fb35d09c83f78f44bf71e8be309206f19fc7c2fc89c41324d65b4c91aff5f81cc47b2ec3e37'
