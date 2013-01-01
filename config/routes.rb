99fa4fc4f87203d5a3ea93ce249ddadfc4ed62eb
johndavid400 <johndavid400@gmail.com>
2013-01-01 16:21:13 -0600

    fixed home page added high voltage

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Deltafin::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  root :to => 'high_voltage/pages#show', :id => 'index'
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
